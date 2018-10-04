bb258837c204fc38ecf10940997f916b79ae676f
Andy Lowry <andy.work@nglowry.com>
2018-10-04 10:28:14 -0400

    ZEN-4452 Fix problems with shader config
    We need to use a resource transformer for the META-INF/services files
    that are scanned by the service loader at runtime to locate available
    GenTemplates. Each GenTemplate project will have one, and we need them
    all. Fortunately, the resource appender implementation that comes with
    the shader jar is just fine for this.
    
    Other resources need to be split out into folders named for the
    project's main packages, now that we can no longer rely on the isolation
    provided by bundle classloaders. So this has been done for the
    OpenApi3Doc gentemplate.

--- modules/gentemplates/openapi3-doc/src/main/java/com/reprezen/genflow/openapi3/doc/OpenApi3DocGenTemplate.java
+++ modules/gentemplates/openapi3-doc/src/main/java/com/reprezen/genflow/openapi3/doc/OpenApi3DocGenTemplate.java
@@ -16,8 +16,8 @@ public class OpenApi3DocGenTemplate extends OpenApi3GenTemplate {
 		// Primary var is defined in GenTemplate.resolve() and is
 		// OpenApi3.toFirstLower()
 		define(outputItem().named("HTML").using(GenerateOpenApi3Doc.class).writing("${openApi3.info.title}_doc.html"));
-		define(staticResource().copying("resources/js").to("."));
-		define(staticResource().copying("resources/images").to("images"));
+		define(staticResource().copying("com/reprezen/genflow/openapi3/doc/js").to("."));
+		define(staticResource().copying("com/reprezen/genflow/openapi3/doc/images").to("images"));
 		define(parameter().named(SHOW_ALL_OF_COMPONENTS_OPTION).optional().withDefault("true")
 				.withDescription("Set to false to suppress component model names in allOf models"));
 		define(parameter().named(INCLUDE_TABLE_OF_CONTENTS_OPTION).optional().withDefault("true")
