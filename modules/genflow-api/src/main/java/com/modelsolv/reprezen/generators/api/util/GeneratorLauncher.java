0975dc8fdd3674c166db4e7081ec394d59640c9a
Andy Lowry <andy.work@nglowry.com>
2018-10-15 17:21:45 -0400

    ZEN-4455 Move compat launcher to genflow
    This was previously in API Studio, but it should be here instead, so
    that properly configured projects can work in old API Studio releases.
    
    The compat class is cmr.generators.api.util.GeneratorLauncher, which
    just delegates to the genflow class

---
+++ modules/genflow-api/src/main/java/com/modelsolv/reprezen/generators/api/util/GeneratorLauncher.java
@@ -0,0 +1,7 @@
+package com.modelsolv.reprezen.generators.api.util;
+
+public class GeneratorLauncher {
+	public static void main(String[] args) {
+		com.reprezen.genflow.api.util.GeneratorLauncher.main(args);
+	}
+}
