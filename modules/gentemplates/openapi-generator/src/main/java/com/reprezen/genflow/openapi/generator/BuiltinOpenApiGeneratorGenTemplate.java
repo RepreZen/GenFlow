9cbe62cf0a0815d96a6ab13e08539c3c12a40070
Andy Lowry <andy.work@nglowry.com>
2018-10-30 12:53:00 -0400

    ZEN-4458 Just did some reformatting - no code changes

--- modules/gentemplates/openapi-generator/src/main/java/com/reprezen/genflow/openapi/generator/BuiltinOpenApiGeneratorGenTemplate.java
+++ modules/gentemplates/openapi-generator/src/main/java/com/reprezen/genflow/openapi/generator/BuiltinOpenApiGeneratorGenTemplate.java
@@ -17,94 +17,95 @@ import com.reprezen.genflow.openapi.generator.OpenApiGeneratorModulesInfo.Parame
 
 public class BuiltinOpenApiGeneratorGenTemplate extends OpenApiGeneratorGenTemplate {
 
-    private final OpenApiGeneratorModulesInfo.Info info;
+	private final OpenApiGeneratorModulesInfo.Info info;
 
-    public BuiltinOpenApiGeneratorGenTemplate(Class<? extends CodegenConfig> codegenClass,
-            OpenApiGeneratorModulesInfo.Info info) {
-        super(codegenClass, info);
-        this.info = info;
-    }
+	public BuiltinOpenApiGeneratorGenTemplate(Class<? extends CodegenConfig> codegenClass,
+			OpenApiGeneratorModulesInfo.Info info) {
+		super(codegenClass, info);
+		this.info = info;
+	}
 
-    @Override
-    public String getName() {
-        return getPreferredName(codegenClass, info);
-    }
+	@Override
+	public String getName() {
+		return getPreferredName(codegenClass, info);
+	}
 
-    @Override
-    public String getId() {
-        return getClass().getPackage().getName() + "." + codegenClass.getSimpleName();
-    }
+	@Override
+	public String getId() {
+		return getClass().getPackage().getName() + "." + codegenClass.getSimpleName();
+	}
 
-    @Override
-    public void configure() throws GenerationException {
-        if (info != null) {
-            for (Parameter param : info.getParameters()) {
-                define(parameter().named(param.getName()).required(param.isRequired())
-                        .withDescription(param.getDescription()).withDefault(param.getDefaultValue()));
-            }
-        }
-        super.configure();
-    }
+	@Override
+	public void configure() throws GenerationException {
+		if (info != null) {
+			for (Parameter param : info.getParameters()) {
+				define(parameter().named(param.getName()).required(param.isRequired())
+						.withDescription(param.getDescription()).withDefault(param.getDefaultValue()));
+			}
+		}
+		super.configure();
+	}
 
-    public static String getPreferredName(Class<? extends CodegenConfig> codegenClass, Info info) {
-        if (info != null) {
-            if (info.getDisplayName() != null) {
-                return info.getDisplayName().trim();
-            } else if (info.getDerivedDisplayName() != null) {
-                return info.getDerivedDisplayName().trim();
-            }
-            // metadata is deficient (shouldn't happen) ... do a discovery-like name generation
-        }
-        try {
-            return getDerivedName(codegenClass.newInstance());
-        } catch (InstantiationException | IllegalAccessException e) {
-            return "(unknown)";
-        }
-    }
+	public static String getPreferredName(Class<? extends CodegenConfig> codegenClass, Info info) {
+		if (info != null) {
+			if (info.getDisplayName() != null) {
+				return info.getDisplayName().trim();
+			} else if (info.getDerivedDisplayName() != null) {
+				return info.getDerivedDisplayName().trim();
+			}
+			// metadata is deficient (shouldn't happen) ... do a discovery-like name
+			// generation
+		}
+		try {
+			return getDerivedName(codegenClass.newInstance());
+		} catch (InstantiationException | IllegalAccessException e) {
+			return "(unknown)";
+		}
+	}
 
-    public static String getDerivedName(CodegenConfig instance) {
-        if (instance == null) {
-            return "(unknown)";
-        }
+	public static String getDerivedName(CodegenConfig instance) {
+		if (instance == null) {
+			return "(unknown)";
+		}
 
-        String name = instance.getClass().getSimpleName();
-        name = trimFromEnd(name, "Generator");
-        name = trimFromEnd(name, "Codegen");
+		String name = instance.getClass().getSimpleName();
+		name = trimFromEnd(name, "Generator");
+		name = trimFromEnd(name, "Codegen");
 
-        CodegenType type = instance != null ? instance.getTag() : null;
-        if (type == null) {
-            type = CodegenType.OTHER;
-        }
-        switch (type) {
-        case CLIENT:
-            name = trimFromEnd(name, "Client") + " Client";
-            break;
-        case SERVER:
-            name = trimFromEnd(name, "Server") + " Server";
-            break;
-        case DOCUMENTATION:
-            name = trimFromEnd(name, "Doc", "Documentation") + " Documentation";
-            break;
-        case CONFIG:
-            name = trimFromEnd(name, "Config", "Configuration") + " Configuration";
-            break;
-        case OTHER:
-        default:
-            name = trimFromEnd(name, "Client", "Server", "Doc", "Documentation", "Config", "Confiuration");
-            break;
-        }
-        name = camelToNatural(name);
-        return name;
-    }
+		CodegenType type = instance != null ? instance.getTag() : null;
+		if (type == null) {
+			type = CodegenType.OTHER;
+		}
+		switch (type) {
+		case CLIENT:
+			name = trimFromEnd(name, "Client") + " Client";
+			break;
+		case SERVER:
+			name = trimFromEnd(name, "Server") + " Server";
+			break;
+		case DOCUMENTATION:
+			name = trimFromEnd(name, "Doc", "Documentation") + " Documentation";
+			break;
+		case CONFIG:
+			name = trimFromEnd(name, "Config", "Configuration") + " Configuration";
+			break;
+		case OTHER:
+		default:
+			name = trimFromEnd(name, "Client", "Server", "Doc", "Documentation", "Config", "Confiuration");
+			break;
+		}
+		name = camelToNatural(name);
+		return name;
+	}
 
-    private static String trimFromEnd(String s, String... suffixes) {
-        for (String suffix : suffixes) {
-            s = s.endsWith(suffix) ? s.substring(0, s.length() - suffix.length()) : s;
-        }
-        return s.trim();
-    }
+	private static String trimFromEnd(String s, String... suffixes) {
+		for (String suffix : suffixes) {
+			s = s.endsWith(suffix) ? s.substring(0, s.length() - suffix.length()) : s;
+		}
+		return s.trim();
+	}
 
-    private static String camelToNatural(String s) {
-        return s.replaceAll("(\\p{Lower})(\\p{Upper})", "$1 $2");
-    }
+	private static String camelToNatural(String s) {
+		return s.replaceAll("(\\p{Lower})(\\p{Upper})", "$1 $2");
+	}
 }
--- modules/gentemplates/rapidml-diagram/src/main/java/com/reprezen/genflow/rapidml/diagram/D3JSPlugin.java
+++ modules/gentemplates/rapidml-diagram/src/main/java/com/reprezen/genflow/rapidml/diagram/D3JSPlugin.java
@@ -19,25 +19,25 @@ import org.osgi.framework.BundleContext;
  */
 public class D3JSPlugin extends Plugin {
 
-    /** The plug-in ID */
-    public static final String PLUGIN_ID = "com.modelsolv.reprezen.gentemplates.d3js"; //$NON-NLS-1$
+	/** The plug-in ID */
+	public static final String PLUGIN_ID = "com.modelsolv.reprezen.gentemplates.d3js"; //$NON-NLS-1$
 
-    /** The shared instance */
-    private static D3JSPlugin plugin;
+	/** The shared instance */
+	private static D3JSPlugin plugin;
 
-    @Override
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        plugin = this;
-    }
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
 
-    @Override
-    public void stop(BundleContext context) throws Exception {
-        plugin = null;
-        super.stop(context);
-    }
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
 
-    public static D3JSPlugin getDefault() {
-        return plugin;
-    }
+	public static D3JSPlugin getDefault() {
+		return plugin;
+	}
 }
