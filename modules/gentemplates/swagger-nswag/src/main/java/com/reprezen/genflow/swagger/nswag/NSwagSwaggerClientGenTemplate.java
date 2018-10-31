ea4818c83e0d6820c9184965f611d8431fa102c9
Andy Lowry <andy.work@nglowry.com>
2018-10-31 12:08:00 -0400

    ZEN-4458 Migrated NSwag gentemplates
    Modified logic for locating nswag executable, since the old logic was
    tied into API Studio.
    
    Before:
    * During gentarget creation, if the relevant API Studio preference
      was set, it was coded into the gentareget as a parameter
    * During execution, the path coded into the gentarget was interpreted as
      the directory where the `nswag` executable could be found. Otherwise,
      it was assumed that `nswag` could be executed via PATH
    
    Now:
    * No path is ever set into the generated gentarget file, but the user
      can edit that as desired.
    * At run time, if the gentarget parameter is present, it's used as
      above. Otherwise, if a specific Java system property is set, that
      value is used as the directory containing `nswag`. If both of these
      options fail, the PATH variable should include the nswag location.
    * API Studio, when launching a gentemplate, now always provides the
      current value of the nswag location preference, if set, using the Java
      system variable in the constructed launch configuration.

---
+++ modules/gentemplates/swagger-nswag/src/main/java/com/reprezen/genflow/swagger/nswag/NSwagSwaggerClientGenTemplate.java
@@ -0,0 +1,59 @@
+package com.reprezen.genflow.swagger.nswag;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.reprezen.genflow.api.GenerationException;
+import com.reprezen.genflow.api.swagger.SwaggerGenTemplate;
+import com.reprezen.genflow.api.template.GenTemplate;
+import com.reprezen.genflow.api.template.GenTemplateContext;
+import com.reprezen.genflow.api.template.GenTemplateProperty;
+import com.reprezen.genflow.swagger.nswag.Config.Output;
+
+import io.swagger.models.Swagger;
+
+public class NSwagSwaggerClientGenTemplate extends SwaggerGenTemplate {
+
+    @Override
+    public boolean isSuppressed() {
+        return !NSwagGenerator.isPlatformSupported();
+    }
+
+    @Override
+    public String getName() {
+        return "C# Client by NSwag";
+    }
+
+    @Override
+    protected void configure() throws GenerationException {
+        alsoKnownAs("com.modelsolv.reprezen.gentemplates.nswag.NSwagSwaggerClientGenTemplate");
+        defineSwaggerSource();
+        Config.defineGenTemplateParameters(this, Config.Output.CS_CLIENT);
+        define(GenTemplateProperty.nswagProvider());
+    }
+
+    @Override
+    protected StaticGenerator<Swagger> getStaticGenerator() {
+        return new Generator(this, context);
+    }
+
+    private static class Generator extends GenTemplate.StaticGenerator<Swagger> {
+
+        public Generator(GenTemplate<Swagger> genTemplate, GenTemplateContext context) {
+            super(genTemplate, context);
+        }
+
+        @Override
+        public void generate(Swagger swagger) throws GenerationException {
+            Config config = Config.fromContext(context);
+            File outputFile = new File(context.getOutputDirectory(), swagger.getInfo().getTitle() + " Client.cs");
+
+            try {
+                new NSwagGenerator(config).generate(config.getNSwagRunFile(swagger, Output.CS_CLIENT, outputFile));
+            } catch (IOException e) {
+                throw new GenerationException("Failed to run NSwag Generation", e);
+            }
+        }
+
+    }
+}
---
+++ modules/gentemplates/swagger-nswag/src/main/java/com/reprezen/genflow/swagger/nswag/NSwagSwaggerServerGenTemplate.java
@@ -0,0 +1,58 @@
+package com.reprezen.genflow.swagger.nswag;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.reprezen.genflow.api.GenerationException;
+import com.reprezen.genflow.api.swagger.SwaggerGenTemplate;
+import com.reprezen.genflow.api.template.GenTemplate;
+import com.reprezen.genflow.api.template.GenTemplateContext;
+import com.reprezen.genflow.api.template.GenTemplateProperty;
+import com.reprezen.genflow.swagger.nswag.Config.Output;
+
+import io.swagger.models.Swagger;
+
+public class NSwagSwaggerServerGenTemplate extends SwaggerGenTemplate {
+
+    @Override
+    public boolean isSuppressed() {
+        return !NSwagGenerator.isPlatformSupported();
+    }
+
+    @Override
+    public String getName() {
+        return "C# Server for ASP.NET Web API 2.0 by NSwag";
+    }
+
+    @Override
+    protected void configure() throws GenerationException {
+        alsoKnownAs("com.modelsolv.reprezen.gentemplates.nswag.NSwagSwaggerServerGenTemplate");
+        defineSwaggerSource();
+        Config.defineGenTemplateParameters(this, Config.Output.CS_SERVER);
+        define(GenTemplateProperty.nswagProvider());
+    }
+
+    @Override
+    protected StaticGenerator<Swagger> getStaticGenerator() {
+        return new Generator(this, context);
+    }
+
+    private static class Generator extends GenTemplate.StaticGenerator<Swagger> {
+
+        public Generator(GenTemplate<Swagger> genTemplate, GenTemplateContext context) {
+            super(genTemplate, context);
+        }
+
+        @Override
+        public void generate(Swagger swagger) throws GenerationException {
+            Config config = Config.fromContext(context);
+            File outputFile = new File(context.getOutputDirectory(), swagger.getInfo().getTitle() + "Controller.cs");
+            try {
+                new NSwagGenerator(config).generate(config.getNSwagRunFile(swagger, Output.CS_SERVER, outputFile));
+            } catch (IOException e) {
+                throw new GenerationException("Failed to run NSwag Generation", e);
+            }
+        }
+
+    }
+}
